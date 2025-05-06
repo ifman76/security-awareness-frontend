d4663e2ea82f6169408df8ab3a6e4f61a39aaa25
ifman76 <ifman76@gmail.com>
2025-05-06 14:31:32 +0900

    feat: add routing and pages for full app flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import ParticipantInfoPage from './pages/ParticipantInfoPage';
+import KnowledgePage from './pages/KnowledgePage';
+import DevicePage from './pages/DevicePage';
+import CuriosityPage from './pages/CuriosityPage';
+import ResultPage from './pages/ResultPage';
 
 export default function App() {
-  return <h1>Security Awareness App 시작</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/info" element={<ParticipantInfoPage />} />
+      <Route path="/knowledge" element={<KnowledgePage />} />
+      <Route path="/device" element={<DevicePage />} />
+      <Route path="/curiosity" element={<CuriosityPage />} />
+      <Route path="/result" element={<ResultPage />} />
+    </Routes>
+  );
 }
