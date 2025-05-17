46103b5a75eee758216e5451655db524707fe014
ifman76 <ifman76@gmail.com>
2025-05-17 20:43:03 +0900

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import AdminPage from './pages/AdminPage';
+import AdminResponsesPage from './pages/AdminResponsesPage';
+
 // 기타 페이지들 import...
 
+<Route path="/admin/responses" element={<AdminResponsesPage />} />
 export default function App() {
   return (
     <Routes>
