df2a295fd2444753a46c3a72b1523c4a6d422334
ifman76 <ifman76@gmail.com>
2025-06-15 16:06:03 +0900

    app 경로 복원

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import AdminPage from './pages/AdminPage';
 import AdminQuestionStatsPage from './pages/AdminQuestionStatsPage'; // ✅ 추가
 import ParticipantInfoPage from './pages/ParticipantInfoPage'; // ✅ 추가
 import AdminResponsesPage from './pages/AdminResponsesPage';
+import KnowledgePage from './pages/KnowledgePage';
+import DevicePage from './pages/DevicePage';
+import CuriosityPage from './pages/CuriosityPage';
+import ResultPage from './pages/ResultPage';
 import PilotFeedbackPage from './pages/PilotFeedbackPage';
 
 
@@ -15,6 +19,10 @@ export default function App() {
     <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/info" element={<ParticipantInfoPage />} /> {/* ✅ 추가 */}
+      <Route path="/knowledge" element={<KnowledgePage />} />
+      <Route path="/device" element={<DevicePage />} />
+      <Route path="/curiosity" element={<CuriosityPage />} />
+      <Route path="/result" element={<ResultPage />} />
       {/* 기타 사용자 페이지들 */}
       <Route path="/admin/responses" element={<AdminResponsesPage />} /> 
       <Route path="/admin" element={<AdminPage />} />
